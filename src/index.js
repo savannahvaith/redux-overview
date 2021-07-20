5626962be4eababd63e24dac2b3e565beb4a2b7b
savannahvaith <sav.vaithilingam@gmail.com>
2021-07-20 13:36:42 +0100

    SV: Interacting with the store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import store from './store';
+
+// console.log(store);
+// console.log(store.getState()); // []
+
+// Subscribe to the store so we get notified when the state changes!
+const unsubscribe = store.subscribe(() => {
+    console.log(`Store changed!`, store.getState());
+});
+
+store.dispatch({
+    type: "bugAdded",
+    payload:{
+        description: "Bug1"
+    }
+})
+
+unsubscribe();
+
+store.dispatch({
+    type:"bugRemoved",
+    payload:{
+        id:1
+    }
+});
+
+
